bfdcf49f313e9c24cf7837c8734d44f033c1ae8d
Lehvy <amaugolevi@gmail.com>
2023-12-05 11:32:33 +0100

    pig game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,19 @@
-'use strict';
+var scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn .btn--roll').addEventListener('click', function() {
+    
+var dice = Math.floor(Math.random() * 6) + 1;
+
+var diceDOM = document.querySelector('.dice');
+diceDOM.style.display = 'block';
+diceDOM.src = 'dice-' + dice + '.png';
+
+
+}); 
+ 
